bb67dd7d7274658ff5044376f1e66dc1b3bde6c5
YeonWon123 <46421507+YeonWon123@users.noreply.github.com>
2021-01-21 11:06:19 +0900

    테스트를 위한 영화 리뷰 데이터 추가
    - ReviewRepository 인터페이스를 repository 패키지에 추가
    - test 폴더 아래에 ReviewRepositoryTests 클래스 추가
    - 테스트 결과 200개의 영화 리뷰가 무작위로 생성됨을 확인
    - 테스트 결과 중간에 리뷰가 없는 영화도 존재함

---
+++ Part 04. Movie Review (영화 리뷰 사이트)/mreview/src/test/java/org/zerock/mreview/repository/ReviewRepositoryTests.java
@@ -0,0 +1,42 @@
+package org.zerock.mreview.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.zerock.mreview.entity.Member;
+import org.zerock.mreview.entity.Movie;
+import org.zerock.mreview.entity.Review;
+
+import java.util.stream.IntStream;
+
+// 100개의 영화(movie 테이블)와 100명의 회원(m_member)이 존재
+// 이를 이용하여 1~5점까지의 평점과 리뷰 내용을 등록하는 테스트 코드
+@SpringBootTest
+public class ReviewRepositoryTests {
+
+    @Autowired
+    private ReviewRepository reviewRepository;
+    
+    @Test
+    public void insertMovieReviews() {
+        // 200개의 리뷰를 동록
+        IntStream.rangeClosed(1, 200).forEach(i -> {
+            // 영화 번호
+            Long mno = (long)(Math.random() * 100) + 1;
+
+            // 리뷰어 번호
+            Long mid = ((long)(Math.random() * 100) + 1);
+            Member member = Member.builder().mid(mid).build();
+
+            Review movieReview = Review.builder()
+                    .member(member)
+                    .movie(Movie.builder().mno(mno).build())
+                    .grade((int)(Math.random() * 5) + 1)
+                    .text("이 영화에 대한 느낌..." + i)
+                    .build();
+
+            reviewRepository.save(movieReview);
+        });
+    }
+
+}
