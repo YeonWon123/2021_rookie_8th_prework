2ad3f092c994c2f81b02865712cc35967823f0e2
YeonWon123 <46421507+YeonWon123@users.noreply.github.com>
2021-01-21 16:21:57 +0900

    업로드 결과 반환, 화면 처리 완료
    - UploadResultDTO 클래스 생성, getImageURL() 메소드 작성
    - UploadController 클래스에서 uploadFile() 메소드의 리턴 타입을 void에서 ResponseEntity<List<UploadResultDTO>>로 변경함
    - 이미지가 아닌 파일의 경우 예외 처리 대신 '403 Forbidden'을 반환하도록 변경
    - 브라우저는 업로드 처리 후 JSON의 배열 형태로 결과를 전달받음
    
    - UploadController 클래스에 getFile() 메서드 생성, URL 인코딩된 파일 이름을 파라미터로 받아서, 해당 파일을 byte[]로 만들어서 브라우저로 전송.
    - 파일의 확장자에 따라서 브라우저에 전송하는 MINE 타입이 달라져야 하는 문제 해결 (java.nio.file 패키지의 Files.probeContentType()을 이용하여 처리)
    - 파일 데이터의 처리는 스프링에서 제공하는 org.springframework.util.FileCopyUtils를 이용해서 처리
    - 브라우저에는 업로드된 결과 중 imageURL 속성이 존재함 (UploadResultDTO의 getImageURL())
    - uploadEx.html에 업로드된 이미지들을 보여줄 수 있는 <div>를 추가하였음
    - showUploadedImages() 함수 작성 : Ajax 업로드 이후 이미지들을 호출함, Ajax 호출 성공 이후 '/display?fileName=xxx'를 호출
    - 테스트 결과, 화면에서 파일을 선택하고 업로드 이후에 다시 브라우저를 통해서 업로드된 파일 조회 가능

--- Part_04_Movie_Review/mreview/src/main/java/org/zerock/mreview/controller/UploadController.java
+++ Part_04_Movie_Review/mreview/src/main/java/org/zerock/mreview/controller/UploadController.java
@@ -2,16 +2,26 @@ package org.zerock.mreview.controller;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.zerock.mreview.dto.UploadResultDTO;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URLDecoder;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -22,13 +32,15 @@ public class UploadController {
     private String uploadPath;
 
     @PostMapping("/uploadAjax")
-    public void uploadFile(MultipartFile[] uploadFiles) {
+    public ResponseEntity<List<UploadResultDTO>> uploadFile(MultipartFile[] uploadFiles) {
+
+        List<UploadResultDTO> resultDTOList = new ArrayList<>();
 
         for (MultipartFile uploadFile: uploadFiles) {
             // 이미지 파일만 업로드 가능
             if(uploadFile.getContentType().startsWith("image") == false) {
                 log.warn("this file is not image type");
-                return;
+                return new ResponseEntity<>(HttpStatus.FORBIDDEN);
             }
 
             // 실제 파일 이름 IE나 Edge는 전체 경로가 들어오므로
@@ -44,21 +56,22 @@ public class UploadController {
 
             // 저장할 파일 이름 중간에 "_"를 이용해서 구분
             String saveName = uploadPath + File.separator + folderPath + File.separator + uuid + "_" + fileName;
-
             Path savePath = Paths.get(saveName);
 
             try {
-                uploadFile.transferTo(savePath);
+                uploadFile.transferTo(savePath); // 실제 이미지 저장
+                resultDTOList.add(new UploadResultDTO(fileName, uuid, folderPath));
             } catch (IOException e) {
                 e.printStackTrace();
             }
 
         } // end for
+        return new ResponseEntity<>(resultDTOList, HttpStatus.OK);
     }
 
     private String makeFolder() {
         String str = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd"));
-        String folderPath = str.replace("//", File.separator);
+        String folderPath = str.replace("/", File.separator);
 
         // make folder --------
         File uploadPathFolder = new File(uploadPath, folderPath);
@@ -67,5 +80,31 @@ public class UploadController {
             uploadPathFolder.mkdirs();
         }
         return folderPath;
+    }
+
+    // URL 인코딩된 파일 이름을 파라미터로 받아서, 해당 파일을 byte[]로 만들어서 브라우저로 전송함
+    // 파일의 확장자에 따라서 브라우저에 전송하는 MINE 타입이 달라져야 하는 문제 : java.nio.file 패키지의 Files.probeContentType()을 이용하여 처리
+    // 파일 데이터의 처리 : 스프링에서 제공하는 org.springframework.util.FileCopyUtils을 이용하여 처리
+    // 브라우저 : 업로드된 결과 중 imageURL 속성 존재
+    @GetMapping("/display")
+    public ResponseEntity<byte[]> getFile(String fileName) {
+        ResponseEntity<byte[]> result = null;
+
+        try {
+            String srcFileName = URLDecoder.decode(fileName, "UTF-8");
+            log.info("fileName: " + srcFileName);
+            File file = new File(uploadPath + File.separator + srcFileName);
+            log.info("file: " + file);
+            HttpHeaders header = new HttpHeaders();
+
+            // MIME타입 처리
+            header.add("Content-Type", Files.probeContentType(file.toPath()));
+            // 파일 데이터 처리
+            result = new ResponseEntity<>(FileCopyUtils.copyToByteArray(file), header, HttpStatus.OK);
+        } catch(Exception e) {
+            log.error(e.getMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return result;
     }
 }
