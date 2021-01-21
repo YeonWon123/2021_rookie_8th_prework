1b18ba9fd691bab330f9b28be9baf6cf427ad4b6
YeonWon123 <46421507+YeonWon123@users.noreply.github.com>
2021-01-21 17:02:36 +0900

    섬네일 이미지 생성과 화면 처리 완료 - 업로드된 파일을 저장하고 섬네일 라이브러리를 활용해서 섬네일 파일을 만들었음 - 섬네일 파일은 파일의 맨 앞에 's_'를 붙여서 일반 파일과 구분함 - UploadResultDTO에 getThumbnailURL()을 추가해서, 섬네일의 경로를 <img> 태그로 처리함
    - build.gradle 파일에 Thumbnailator 추가
    - UploadController에서 net.coobird.thumbnailator.Thumbnailator를 import하고, uploadFile() 수정 (섬네일 파일 이름을 중간에 s_로 시작하도록 수정
    - UploadResultDTO 클래스에 getThumbnailURL() 메서드 생성
    - uploadEx.html에서 섬네일 이미지를 보여주도록 showUploadImages() 수정
    
    - 테스트 결과 파일 업로드 결과를 브라우저에서 보여줄 떄, 섬네일 이미지들만 추가되는 것을 확인할 수 있었음
    - 테스트 결과 업로드된 파일들의 이미지 비율도 자동으로 조정된 결과 확인함

--- Part_04_Movie_Review/mreview/src/main/java/org/zerock/mreview/dto/UploadResultDTO.java
+++ Part_04_Movie_Review/mreview/src/main/java/org/zerock/mreview/dto/UploadResultDTO.java
@@ -14,9 +14,21 @@ public class UploadResultDTO implements Serializable {
     private String uuid;
     private String folderPath;
 
+    // 이미지 링크를 처리하기 위한 메서드
     public String getImageURL() {
         try {
             return URLEncoder.encode(folderPath+"/"+uuid+"_"+fileName, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    // 섬네일 링크를 처리하기 위한 메서드
+    // 업로드된 파일과 동일한 이름에 's_'가 붙은 형태이므로 구분이 어렵지 않음
+    public String getThumbnailURL() {
+        try {
+            return URLEncoder.encode(folderPath+"/s_"+uuid+"_"+fileName, "UTF-8"); // "/s_" 추가
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         }
